49579ee256d6cfa33f7095bdfc87e44eaab60624
rachelblumsnow <68304022+rachelblumsnow@users.noreply.github.com>
2022-02-12 16:11:28 -0500

    Update dcr_ui.py

--- streamlit_prophet/app/dcr_ui.py
+++ streamlit_prophet/app/dcr_ui.py
@@ -34,7 +34,7 @@ sideb.header ("Account Login")
 sideb.text_input('Snowflake Account', value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, placeholder=None)
 sideb.text_input('User Name', value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, placeholder=None)
 sideb.text_input('Password', value="", max_chars=None, key=None, type="password", help=None, autocomplete=None, on_change=None, placeholder=None)
-st.button("Submit", key=None, help=None, on_click=None, args=None, kwargs=None)
+sideb.button("Submit", key=None, help=None, on_click=None, args=None, kwargs=None)
 
 # Sidebar choose page 
 sideb.header ("Configuration Navigation")
@@ -74,7 +74,7 @@ def run_query2(query_text):
             
 run_query("select template_name from DCR_DEMO_APP.CLEANROOM.TEMPLATES;")
 
-st.button("Submit", key=None, help=None, on_click=None, args=None, kwargs=None)
+   st.button("Submit", key=None, help=None, on_click=None, args=None, kwargs=None)
 
       
       
