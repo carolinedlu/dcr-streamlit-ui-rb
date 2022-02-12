e6e760d0ae27a48c9bdd911ea61c238d7a528d77
rachelblumsnow <68304022+rachelblumsnow@users.noreply.github.com>
2022-02-12 14:11:04 -0500

    Update provider_admin.py

--- streamlit_prophet/app/pages/provider_admin.py
+++ streamlit_prophet/app/pages/provider_admin.py
@@ -1,2 +1,31 @@
-# Provider Admin Main Page
-st.title("Provider Admin Main Page")
+from typing import Any, Dict, List
+
+import streamlit as st
+import snowflake.connector
+import plotly.figure_factory as ff
+import numpy as np
+import altair as alt
+
+
+from streamlit_prophet.lib.utils.load import load_config, load_image
+
+# Page config
+#favicon=st.image(load_image("Darkpoolwhite.png"))
+st.set_page_config(page_title="snowdcr",page_icon="❄️")
+
+# Load config
+config, instructions, readme = load_config(
+   "config_streamlit.toml", "config_instructions.toml", "config_readme.toml"
+)
+
+# Initialization
+dates: Dict[Any, Any] = dict()
+report: List[Dict[str, Any]] = []
+   
+
+# CONSUMER REQUEST Main Page
+# MainPageLogo
+# st.image(load_image("Darkpoolwhite.png"), use_column_width=True)
+# Main Page Header
+
+st.title("Provider Admin")
