1edd2447c47932ed8e81b1924761415ec9c02dc6
rachelblumsnow <68304022+rachelblumsnow@users.noreply.github.com>
2022-02-12 13:18:59 -0500

    Update dcr_ui.py

--- streamlit_prophet/app/dcr_ui.py
+++ streamlit_prophet/app/dcr_ui.py
@@ -25,7 +25,7 @@ report: List[Dict[str, Any]] = []
 # Sidebar
 sideb = st.sidebar
 sideb.image(load_image("DCRLogoGray.png"),use_column_width=True)
-sideb.text_input('Snowflake Account', 'Placeholder', max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, *, placeholder=None, disabled=False)
+sideb.text_input('Snowflake Account', value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, placeholder='placeholder', disabled=False)
 # st.sidebar.text_input('User Name', value="placeholder", max_chars=None, key=None, type="str", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, *, placeholder=None, disabled=False)
 # st.sidebar.text_input('Password', value="placeholder", max_chars=None, key=None, type="password", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, *, placeholder=None, disabled=False)
 
