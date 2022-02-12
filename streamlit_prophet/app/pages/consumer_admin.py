0e1d2b6621768c1560529d81a30d29330cf931e7
rachelblumsnow <68304022+rachelblumsnow@users.noreply.github.com>
2022-02-12 14:12:00 -0500

    Update consumer_admin.py

--- streamlit_prophet/app/pages/consumer_admin.py
+++ streamlit_prophet/app/pages/consumer_admin.py
@@ -11,10 +11,6 @@ import altair as alt
 
 from streamlit_prophet.lib.utils.load import load_config, load_image
 
-# Page config
-#favicon=st.image(load_image("Darkpoolwhite.png"))
-st.set_page_config(page_title="snowdcr",page_icon="❄️")
-
 # Load config
 config, instructions, readme = load_config(
    "config_streamlit.toml", "config_instructions.toml", "config_readme.toml"
