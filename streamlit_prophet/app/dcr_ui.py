80be7d55527696ef568c022c0b7f737b2ccfc89d
rachelblumsnow <68304022+rachelblumsnow@users.noreply.github.com>
2022-02-12 14:01:00 -0500

    Update dcr_ui.py

--- streamlit_prophet/app/dcr_ui.py
+++ streamlit_prophet/app/dcr_ui.py
@@ -11,6 +11,28 @@ from streamlit_prophet.lib.utils.load import load_config, load_image
 from multipage import MultiPage
 from pages import consumer_request, consumer_admin
 
+# Sidebar
+sideb = st.sidebar
+sideb.image(load_image("DCRLogoGray.png"),use_column_width=True)
+sideb.header ("Account Login")
+sideb.text_input('Snowflake Account', value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, placeholder=None)
+sideb.text_input('User Name', value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, placeholder=None)
+sideb.text_input('Password', value="", max_chars=None, key=None, type="password", help=None, autocomplete=None, on_change=None, placeholder=None)
+
+# Sidebar choose page 
+sideb.header ("Configuration Navigation")
+persona = sideb.selectbox("", ('Consumer Analyst','Consumer Admin','Provider Admin'))
+if persona == 'Consumer Analyst':
+      sideb.write('You are viewing the Consumer Analyst Setup Page.')
+else:
+      sideb.write('This setup page is under construction.') 
+
+      
+if persona == 'Consumer Analyst':
+   st.title("Consumer Request")
+    
+
+
 # Create an instance of the app 
 app = MultiPage()
 
@@ -38,23 +60,3 @@ app.add_page("Consumer Request", consumer_request.py)
 # The main app
 app.run()
    
-# Sidebar
-sideb = st.sidebar
-sideb.image(load_image("DCRLogoGray.png"),use_column_width=True)
-sideb.header ("Account Login")
-sideb.text_input('Snowflake Account', value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, placeholder=None)
-sideb.text_input('User Name', value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, placeholder=None)
-sideb.text_input('Password', value="", max_chars=None, key=None, type="password", help=None, autocomplete=None, on_change=None, placeholder=None)
-
-# Sidebar choose page 
-sideb.header ("Configuration Navigation")
-persona = sideb.selectbox("", ('Consumer Analyst','Consumer Admin','Provider Admin'))
-if persona == 'Consumer Analyst':
-      sideb.write('You are viewing the Consumer Analyst Setup Page.')
-else:
-      sideb.write('This setup page is under construction.') 
-
-      
-if persona == 'Consumer Analyst':
-   st.title("Consumer Request")
-    
